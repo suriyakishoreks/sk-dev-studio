a7f4d2e74776a72bc43f4c7b256768ff4dfd2dac
Suriyakishore K S <kssuriyakishore16@gmail.com>
2025-07-20 22:33:18 +0530

    chore: change domain name in configs

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const BASE_PATH = '/sk-dev-studio';
-
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: BASE_PATH,
   images: {
     unoptimized: true,
   },
@@ -12,6 +9,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-
-// --cname www.mywebsite.com
